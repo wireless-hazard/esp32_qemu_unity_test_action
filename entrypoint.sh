718a5005d8cc9d3d72c9bf5ac1c316c6b8eff7a5
wireless-hazard <55916435+wireless-hazard@users.noreply.github.com>
2022-02-25 22:14:46 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 CODE_PATH="$1"
 . $IDF_PATH/export.sh
 mkdir /project/components/
-cp -r $GITHUB_WORKSPACE/$CODE_PATH/ /project/components/$CODE_PATH/
+cp -r $GITHUB_WORKSPACE/component/ /project/components/$CODE_PATH/
 cd /project/
 ls /project/components/$CODE_PATH/
 idf.py build
